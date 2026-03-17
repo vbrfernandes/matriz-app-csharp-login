7c64947197f388bc4f72809ead6c1c8b42e3b767
Vitor de Barros Rigueira Fernandes <vbrfernandes@sga.pucminas.br>
2026-03-16 21:08:21 -0300

    Commit finais

--- backEnd/MatrizApi/MatrizApi/AppDbContext.cs
+++ backEnd/MatrizApi/MatrizApi/AppDbContext.cs
@@ -11,5 +11,19 @@ namespace MatrizApi
 
         public DbSet<Tarefa> Tarefas { get; set; }
         public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configura o ID para ser auto-incremento real no Postgres
+            modelBuilder.Entity<Usuario>()
+                .Property(u => u.Id)
+                .UseIdentityByDefaultColumn();
+
+            modelBuilder.Entity<Tarefa>()
+                .Property(t => t.Id)
+                .UseIdentityByDefaultColumn();
+        }
     }
 }
